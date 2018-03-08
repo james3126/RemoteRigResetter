201047dab391f11b09c114a0c9777dfff936071b
james3126 <jk3126@hotmail.co.uk>
2018-03-08 09:26:40 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,15 @@
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<meta name="description" content="Offical website for controlling the RRR">
-		<meta name="keywords" content="Raspberry,Pi,RRR,Remote,Rig,Resetter,mining,bitcoin,computer,control,james,jammyworld">
-		<meta name="author" content="James Kerley">
-		<link rel="stylesheet" type="text/css" href="./style.css">
-		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-		<title>RRR - Remote Rig Resetter</title>
-	</head>
-
-	<body>
-
-		<?php require('./header.html') ?>
-		
-		<div id="body-wrapper">
-			<div id="home-wrapper">
-				<div id="home-wrapper-outter">
-					<div id="home-wrapper-inner">
-						<div id="home-inner-title">
-						
-						</div>
-						
-						<div id="home-inner-content-wrapper">
-						
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<style>
-
-		
-		
-		
-		</style>
-	</body>
-</html>
++        <body>
++                <form action="./control.php" method="GET">
++                        Pi [local]: <input type="text" name="pi">
++                        <br>
++                        Command: <input type="text" name="com">
++                        <br>
++                        rigNum: <input type="text" name="rigNum">
++                        <br>
++                        <input type="submit" value="submit">
++                </form>
++        </body>
++
++
++</html>
